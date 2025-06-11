3d569517fd1cc0791e7a73bbf2e55cfa19fa0b07
dmswl6310 <dmswl6310@gmail.com>
2025-06-11 22:30:24 +0900

    feat: 투두리스트 추가기능 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Todo from "./Todo";
 import React, { useState } from "react";
+import { Container, List, Paper } from "@mui/material";
+import AddTodo from "./AddTodo";
 
 function App() {
   const [items, setItems] = useState([
@@ -16,10 +18,31 @@ function App() {
     },
   ]);
 
-  let todoItems =
-    items.length > 0 && items.map((item) => <Todo item={item} key={item.id} />);
+  let todoItems = items.length > 0 && (
+    <Paper style={{ margin: 16 }}>
+      <List>
+        {items.map((item) => (
+          <Todo item={item} key={item.id} />
+        ))}
+      </List>
+    </Paper>
+  );
 
-  return <div className="App">{todoItems}</div>;
+  const addItem = (item) => {
+    item.id = "ID-" + items.length;
+    item.done = false;
+    setItems([...items, item]);
+    console.log("items : ", items);
+  };
+
+  return (
+    <div className="App">
+      <Container maxWidth="md">
+        <AddTodo addItem={addItem} />
+        <div className="TodoList">{todoItems}</div>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
