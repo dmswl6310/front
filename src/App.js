3a3c2b64f73eaed6f7bc75ba970b16d90ac90445
dmswl6310 <dmswl6310@gmail.com>
2025-06-11 21:54:40 +0900

    feat: todo list 아이템 표시

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Todo from "./Todo";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [items, setItems] = useState([
+    {
+      id: "0",
+      title: "Hello world 1",
+      done: true,
+    },
+    {
+      id: "1",
+      title: "Hello world 2",
+      done: true,
+    },
+  ]);
+
+  let todoItems =
+    items.length > 0 && items.map((item) => <Todo item={item} key={item.id} />);
+
+  return <div className="App">{todoItems}</div>;
 }
 
 export default App;
