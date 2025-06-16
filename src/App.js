b8641a021f9bc969068a31452aff912331f8cc7f
dmswl6310 <dmswl6310@gmail.com>
2025-06-16 16:51:01 +0900

    feat: 로그아웃 UI & 네비게이션바 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import Todo from "./Todo";
 import React, { useState, useEffect } from "react";
-import { Container, List, Paper } from "@mui/material";
-import { call } from "./service/ApiService";
+import { Container, List, Paper,Grid,Button,AppBar,Toolbar,Typography } from "@mui/material";
+import { call,signout } from "./service/ApiService";
 import AddTodo from "./AddTodo";
 
 function App() {
@@ -24,6 +24,23 @@ function App() {
     call("/todo", "PUT", item).then((response) => setItems(response.data));
   };
 
+  let navigationBar=(
+    <AppBar position="static">
+      <Toolbar>
+        <Grid jusifyContent="space-between" container>
+          <Grid item>
+            <Typography variant="h6">오늘의 할일</Typography>
+          </Grid>
+          <Grid item>
+            <Button color="inherit" raised onClick={signout}>
+              로그아웃
+            </Button>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+
   let todoItems = items.length > 0 && (
     <Paper style={{ margin: 16 }}>
       <List>
@@ -41,6 +58,7 @@ function App() {
 
   return (
     <div className="App">
+      {navigationBar}
       <Container maxWidth="md">
         <AddTodo addItem={addItem} />
         <div className="TodoList">{todoItems}</div>
