5f62b4b8dc145bc37af0e0c6d44c7736662a35e8
dmswl6310 <dmswl6310@gmail.com>
2025-06-26 20:59:17 +0900

    chore: backendHost 수정

--- src/api-config.js
+++ src/api-config.js
@@ -2,11 +2,11 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
-if (hostname == "localhost") {
+if (hostname === "localhost") {
   backendHost = "http://localhost:8080";
 } else {
   backendHost =
-    "first-prod-on-todo-api-service.ap-northeast-2.elasticbeanstalk.com";
+    "http://first-prod-on-todo-api-service.ap-northeast-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
