d988d9d5a99c854f26899e9efb6afb4faf92b85f
dmswl6310 <dmswl6310@gmail.com>
2025-06-27 14:42:39 +0900

    fix: 백엔드 주소변경

--- src/api-config.js
+++ src/api-config.js
@@ -5,8 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === "localhost") {
   backendHost = "http://localhost:8080";
 } else {
-  backendHost =
-    "http://first-prod-on-todo-api-service.ap-northeast-2.elasticbeanstalk.com";
+  backendHost = "https//api.fullstack-eunji.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
