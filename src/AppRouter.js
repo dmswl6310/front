405b11ca74fdd40155391d4e5351478576ce2d3c
dmswl6310 <dmswl6310@gmail.com>
2025-07-02 17:13:13 +0900

    feat: 소셜로그인 추가, 토큰파싱과저장 추가

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -3,34 +3,36 @@ import "./index.css";
 import App from "./App";
 import Login from "./Login";
 import SignUp from "./SignUp";
-import {BrowserRouter,Routes,Route} from "react-router-dom";
-import { Typography,Box } from "@mui/material"
+import SocialLogin from "./SocialLogin";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Typography, Box } from "@mui/material";
 
-function Copyright(){
-    return(
-        <Typography variant="body2" color="textSecondary" align="center">
-        {"Copyright ⓒ "}
-        eunji, {new Date().getFullYear()}
-        {"."}
-        </Typography>
-    )
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright ⓒ "}
+      eunji, {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
 
-function AppRouter(){
-    return(
-        <div>
-            <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App/>}/>
-                <Route path="login" element={<Login/>}/>
-                <Route path="signup" element={<SignUp/>}/>
-            </Routes>
-            </BrowserRouter>
-            <Box mt={5}>
-                <Copyright/>
-            </Box>
-        </div>
-    )
+function AppRouter() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="sociallogin" element={<SocialLogin />} />
+        </Routes>
+      </BrowserRouter>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </div>
+  );
 }
 
 export default AppRouter;
--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,17 +1,17 @@
 import { API_BASE_URL } from "../api-config";
 
 export function call(api, method, request) {
-  let headers= new Headers({
-      "Content-Type": "application/json",
-    });
-  
-    const accessToken=localStorage.getItem("ACCESS_TOKEN");
-    if(accessToken && accessToken!=null){
-      headers.append("Authorization","Bearer "+accessToken);
-    }
+  let headers = new Headers({
+    "Content-Type": "application/json",
+  });
+
+  const accessToken = localStorage.getItem("ACCESS_TOKEN");
+  if (accessToken && accessToken != null) {
+    headers.append("Authorization", "Bearer " + accessToken);
+  }
 
   let options = {
-    headers:headers,
+    headers: headers,
     url: API_BASE_URL + api,
     method: method,
   };
@@ -24,9 +24,9 @@ export function call(api, method, request) {
     .then((response) => {
       if (response.status === 200) {
         return response.json();
-      }else if(response.status==403){
-        window.location.href="/login"; // redirect
-      }else {
+      } else if (response.status == 403) {
+        window.location.href = "/login"; // redirect
+      } else {
         Promise.reject(response);
         throw Error(response);
       }
@@ -37,22 +37,31 @@ export function call(api, method, request) {
     });
 }
 
-export function signin(userDTO){
-  return call("/auth/signin","POST",userDTO)
-    .then((response)=>{
-      if(response.token){
-        // 로컬스토리지에 토큰 저장
-        localStorage.setItem("ACCESS_TOKEN",response.token);
-        window.location.href="/";
-      }
-    });
-  }
+export function signin(userDTO) {
+  return call("/auth/signin", "POST", userDTO).then((response) => {
+    if (response.token) {
+      // 로컬스토리지에 토큰 저장
+      localStorage.setItem("ACCESS_TOKEN", response.token);
+      window.location.href = "/";
+    }
+  });
+}
 
-  export function signout(){
-    localStorage.setItem("ACCESS_TOKEN",null);
-    window.location.href="./login";
-  }
+export function signout() {
+  localStorage.setItem("ACCESS_TOKEN", null);
+  window.location.href = "./login";
+}
 
-  export function signup(userDTO){
-    return call("/auth/signup","POST",userDTO);
-  }
+export function signup(userDTO) {
+  return call("/auth/signup", "POST", userDTO);
+}
+
+export function socialLogin(provider) {
+  const frontendUrl = window.location.protocol + "//" + window.location.host;
+  window.location.href =
+    API_BASE_URL +
+    "/auth/authorize/" +
+    provider +
+    "?redirect_url=" +
+    frontendUrl;
+}
