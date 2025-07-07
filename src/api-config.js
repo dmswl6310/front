16ce4e3252ed1ef8f2c29ee9c3f13e3147021ca3
dmswl6310 <dmswl6310@gmail.com>
2025-07-08 00:35:13 +0900

    fix: 세미콜론 빠진 url 오류 수정

--- src/api-config.js
+++ src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === "localhost") {
   backendHost = "http://localhost:8080";
 } else {
-  backendHost = "https//api.fullstack-eunji.com";
+  backendHost = "https://api.fullstack-eunji.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
